6f0e97dc0422c5350f531ee3ab2d229c1c2e19d5
njayman <najishm828282@gmail.com>
2020-12-02 11:32:23 +0600

    added logo in auth page

--- src/components/authpartials/Auth.js
+++ src/components/authpartials/Auth.js
@@ -1,11 +1,15 @@
 import React, { Fragment, useState } from 'react'
 import AdminForm from './AdminForm'
 import UserForm from './UserForm'
+import logo from '../../assets/rpgcl_logo.png'
 
 function Auth() {
     const [role, setRole] = useState('user')
     return (
         <Fragment>
+            <div className="authbody__logo">
+                <img src={logo} alt="logo" />
+            </div>
             <div className="authbody__nav">
                 <button className={`authbody__nav__button ${role === 'user' ? "active" : ""}`} value="user" onClick={e => setRole(e.target.value)}>User</button>
                 <button className={`authbody__nav__button ${role === 'admin' ? "active" : ""}`} value="admin" onClick={e => setRole(e.target.value)}>Admin</button>
